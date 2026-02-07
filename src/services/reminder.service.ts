6047006a431c231d2b6a75a66890c3e857bdeffd
saleem-shaik-git <79854677+saleem-shaik-git@users.noreply.github.com>
2026-02-07 02:17:37 +0000

    Update reminder.service.ts

--- src/services/reminder.service.ts
+++ src/services/reminder.service.ts
@@ -1,1 +1,19 @@
+/**
+ * Bin collection reminder service
+ * Designed for local authority / community use
+ *
+ * Author: Saleem Shaik
+ */
+
+type BinCollection = {
+  binType: string;
+  day: string;
+  frequency: string;
+};
+
+export class ReminderService {
+  generateReminder(collection: BinCollection): string {
+    return `Reminder: ${collection.binType} bin will be collected on ${collection.day}.`;
+  }
+}
 

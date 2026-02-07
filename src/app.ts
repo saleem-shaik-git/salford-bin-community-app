9d700e3125b507ceafb3f067973dafbe371e1c82
saleem-shaik-git <79854677+saleem-shaik-git@users.noreply.github.com>
2026-02-07 02:17:49 +0000

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
+/**
+ * Application entry point (conceptual)
+ * Technical Lead: Saleem Shaik
+ */
+
+import { ReminderService } from './services/reminder.service';
+import schedule from './data/bin-schedule.sample.json';
+
+const service = new ReminderService();
+
+schedule.collections.forEach((collection) => {
+  console.log(service.generateReminder(collection));
+});
 
